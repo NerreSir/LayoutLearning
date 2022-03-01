85b5c98acad558c92c740a482d4d53fd75f22359
nerresir <bugraarslan96@gmail.com>
2022-03-01 10:19:28 +0300

    Layout-Turleri

---
+++ app/src/main/java/com/atilsamancioglu/layoutlearning/MainActivity.java
@@ -0,0 +1,26 @@
+package com.atilsamancioglu.layoutlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+
+    LAYOUT TÜRLERİ
+
+    Lineer : öyle düz yatay yada dikey sıralı
+    Relative : bi tık daha özgür (margin ile boşluk bırakmaca)
+    Grid : f1 cars ;) sutün satır mantığı ile sıralanma
+    Frame : öyle tek ekran wallpaper appi falan
+    Constraint : sanırım en rahat ve en kullanışlısı (en son çıkan layout)
+     */
+
+}
